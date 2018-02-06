453d88623899f11453d81c6cf78909d616d7efdd
ducviet1696 <ducviet169614@gmail.com>
2018-02-06 11:46:56 +0700

    Finish!

--- Operator/index.js
+++ Operator/index.js
@@ -1,2 +1,4 @@
 exports.Addition = require('./Addition');
 exports.Subtraction = require('./Subtraction');
+exports.Multiplication = require('./Multiplication');
+exports.Division = require('./Division');
